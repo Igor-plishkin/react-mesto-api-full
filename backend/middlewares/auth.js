da27ec18c37cae826767058bcda5e5a24fb0da82
Igor-plishkin <Plisha.jr@yandex.ru>
2021-08-29 05:54:41 +0500

    fix auth back

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errors/unauthorized-err");
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const { Authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith("Bearer ")) {
+  if (!Authorization || !Authorization.startsWith("Bearer ")) {
     throw new UnauthorizedError("Необходима авторизация");
   }
 
-  const token = authorization.replace("Bearer ", "");
+  const token = Authorization.replace("Bearer ", "");
   let payload;
 
   try {

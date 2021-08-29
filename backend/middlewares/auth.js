212743cae445c4cd72d29a9e357e9ebd04e0e409
Igor-plishkin <Plisha.jr@yandex.ru>
2021-08-29 17:09:09 +0500

    test env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,6 @@ const UnauthorizedError = require("../errors/unauthorized-err");
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { NODE_ENV, JWT_SECRET } = process.env;
-  console.log(JWT_SECRET);
 
   if (!req.cookies.jwt) {
     throw new UnauthorizedError("Необходима авторизация");
@@ -11,35 +10,6 @@ module.exports = (req, res, next) => {
 
   const token = req.cookies.jwt;
   let payload;
-
-  const YOUR_JWT = 'secret-key';
-// вставьте сюда JWT, который вернул публичный сервер студента
-
-const SECRET_KEY_DEV = 'some-special';
-// вставьте сюда секретный ключ для разработки из кода студента
-
-
-try {
-  const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-
-  console.log('\x1b[31m%s\x1b[0m', `
-    Надо исправить. В продакшне используется тот же
-    секретный ключ, что и в режиме разработки.
-  `);
-} catch (err) {
-  if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-    console.log(
-      '\x1b[32m%s\x1b[0m',
-      'Всё в порядке. Секретные ключи отличаются'
-    );
-  } else {
-    console.log(
-      '\x1b[33m%s\x1b[0m',
-      'Что-то не так',
-      err
-    );
-  }
-}
 
   try {
     payload = jwt.verify(token, NODE_ENV === "production" ? JWT_SECRET : "secret-key");
